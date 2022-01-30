290dd46d9c3e7bd2cb5e46d4bd34052aafe5afd1
DeniseFee <d.feeleus@gmail.com>
2022-01-30 12:44:28 +0100

    clean up audio and import music loop
    clean up audio and import music loop

--- audio/audio.gd
+++ audio/audio.gd
@@ -6,16 +6,10 @@ export var transition_duration = 1.00
 export var transition_type = 1 # TRANS_SINE
 var out = true
 
-
-func fade_met_knop():
-    if(out):
-        fade_out()
-    else:
-        fade_in()
-
+func _ready():
+    fade_in()
 
 func fade_out():
-    print("out")
     tween.interpolate_property(
         self, "volume_db",
         0, -80, transition_duration,
@@ -25,14 +19,10 @@ func fade_out():
 
 
 func _on_TweenOut_tween_completed(object, key):
-    # stop the music -- otherwise it continues to run at silent volume
     object.stop()
-    object.volume_db = 0 # reset volume
 
 
 func fade_in():
-    # tween music volume down to 0
-    print("in")
     tween.interpolate_property(
         self, "volume_db",
         -80, 0, transition_duration,
--- handcards/discard-pile.gd
+++ handcards/discard-pile.gd
@@ -8,56 +8,56 @@ var tween
 
 
 func _ready():
-	tween = Tween.new()
-	add_child(tween)
+    tween = Tween.new()
+    add_child(tween)
 
 
 func _on_Root_next_action(turn: String, player: int):
-	if turn != "discard":
-		return
+    if turn != "discard":
+        return
 
-	if player != 0:
-		emit_signal("action_ended", turn, {"skipped": true})
-		return
+    if player != 0:
+        emit_signal("action_ended", turn, {"skipped": true})
+        return
 
-	visible = true
+    visible = true
 
-	discard_hand()
+    discard_hand()
 
-	yield(self, "hand_discarded")
+    yield(self, "hand_discarded")
 
-	visible = false
-	emit_signal("action_ended", turn, {})
+    visible = false
+    emit_signal("action_ended", turn, {})
 
 
 func discard_hand():
-	var cards = get_node(cards_path)
+    var cards = get_node(cards_path)
 
-	for follower in cards.get_children():
-		var card = follower.get_node("HandCard")
+    for follower in cards.get_children():
+        var card = follower.get_node("HandCard")
 
-		var position = card.global_position
-		var rotation = card.global_rotation_degrees
+        var position = card.global_position
+        var rotation = card.global_rotation_degrees
 
-		follower.remove_child(card)
-		cards.remove_child(follower)
-		follower.queue_free()
+        follower.remove_child(card)
+        cards.remove_child(follower)
+        follower.queue_free()
 
-		add_child(card)
-		card.global_position = position
-		card.global_rotation_degrees = rotation
+        add_child(card)
+        card.global_position = position
+        card.global_rotation_degrees = rotation
 
-		tween.interpolate_property(
-			card, "global_position",
-			position, rect_position, 1.0,
-			Tween.TRANS_BACK, Tween.EASE_IN_OUT)
-		tween.interpolate_property(
-			card, "global_rotation_degrees",
-			rotation, 0, 1.0,
-			Tween.TRANS_BACK, Tween.EASE_IN_OUT)
-		tween.start()
+        tween.interpolate_property(
+            card, "global_position",
+            position, rect_position, 1.0,
+            Tween.TRANS_BACK, Tween.EASE_IN_OUT)
+        tween.interpolate_property(
+            card, "global_rotation_degrees",
+            rotation, 0, 1.0,
+            Tween.TRANS_BACK, Tween.EASE_IN_OUT)
+        tween.start()
 
-		yield(get_tree().create_timer(0.5), "timeout")
+        yield(get_tree().create_timer(0.5), "timeout")
 
-	yield(tween, "tween_all_completed")
-	emit_signal("hand_discarded")
+    yield(tween, "tween_all_completed")
+    emit_signal("hand_discarded")
